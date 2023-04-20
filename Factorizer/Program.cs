4f335555b7ebe8863e3b210c8da4182a8c8e8864
Zhiyao LYU <lvzhiyao@hotmail.com>
2023-04-20 16:50:49 -0400

    added 2 projects: hello world and factorizer

---
+++ Factorizer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+This program must be in a new console application named Factorizer.
+This program must ask the user for the number the program will factor.
+The program must print out the original number.
+The program must print out each factor of the number (not including the number itself).
+The program must print out the total number of factors for the number.
+The program must print out whether the number is perfect.
+The program must print out whether the number is prime.
+ */
+
+namespace Factorizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What number would you like to factor? ");
+            int num = int.Parse(Console.ReadLine());
+
+            string strIsPrime = "";
+            string strIsPerfect = "";
+            List<int> factors = new List<int> { 1 };
+            Console.Write("The factors of " + num + " are: 1 ");
+            for (int p = 2; p <= num / 2; p++) {
+                if (num  % p == 0) { 
+                    Console.Write(p + " ");
+                    factors.Add(p);
+                    strIsPrime = "not ";
+                }  
+            }
+            Console.WriteLine(num + ". ");
+
+            Console.WriteLine("{0} has {1} factors, ", num, factors.Count + 1);
+            if (factors.Sum() == num) strIsPerfect = "";
+            Console.WriteLine(num + $" is {strIsPerfect}a perfect number.");
+            Console.WriteLine(num + $" is {strIsPrime}a prime number.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorldSample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace HelloWorldSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+
+            Console.Write("Did you know that in 1440 (or thereabouts),");
+            Console.WriteLine("Johannes Gutenberg invented the printing press with moveable type?");
+            Console.WriteLine("He started out as a goldsmith!");
+
+            Console.Write("4 is greater than -1: ");
+            Console.WriteLine(4 > -1);
+
+            Console.WriteLine("\n****** Now make the computer do some harder math!");
+
+            Console.Write("8043.52 minus 4.2 plus 23.0 divided by 56.0 times -76.13 is: ");
+            Console.WriteLine((8043.52 - 4.2 + 23) / 56 * (-76.13));
+
+            Console.ReadLine();
+        }
+    }
+}
